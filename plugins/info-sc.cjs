e0c5f7895eb7bc6e7fb87e09fc5ecbdaef2c2f32
animek12 <111511065+animek12@users.noreply.github.com>
2022-10-27 20:23:32 +0800

    Update info-sc.cjs

--- plugins/info-sc.cjs
+++ plugins/info-sc.cjs
@@ -1,6 +1,6 @@
 var handler = async(m, { conn, text, usedPrefix, command }) => {
 
-var str = `*https://github.com/FahriAdison/Sad-Bot-V2*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan follow github saya dibawah ,Terimakasih`
+var str = `wa.me/6282261065144`
 conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
 }
 handler.command = handler.help = ['sc']
